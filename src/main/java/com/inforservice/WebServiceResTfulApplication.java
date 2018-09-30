a8c81ca64757c5989195aaea4e6736ab0ee84712
Lindson Amaro <lindson@inforservice-ce.com.br>
2018-09-30 14:54:53 -0300

    primeiro commit

---
+++ src/main/java/com/inforservice/WebServiceResTfulApplication.java
@@ -0,0 +1,12 @@
+package com.inforservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceResTfulApplication.class, args);
+	}
+}
